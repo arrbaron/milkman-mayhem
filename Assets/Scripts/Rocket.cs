f35591fdc1ade73b338af35535d6fd5d2e837ceb
Roy Baron <baron.roy@gmail.com>
2017-11-15 21:24:14 -0500

    add thrust, movement, and audio

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	Rigidbody rigidbody;
+	AudioSource audioSource;
+
+	public int thrustForce = 15;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ProcessInput();
+	}
+
+	void ProcessInput() {
+		if (Input.GetKey(KeyCode.Space)) {
+			rigidbody.AddRelativeForce(Vector3.up * thrustForce);
+			if (!audioSource.isPlaying) audioSource.Play();
+
+		}
+		if (Input.GetKeyUp(KeyCode.Space)) {
+			audioSource.Stop();
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			transform.Rotate(Vector3.forward);
+		}
+		else if (Input.GetKey(KeyCode.D)) {
+			transform.Rotate(-Vector3.forward);
+		}
+	}
+}
